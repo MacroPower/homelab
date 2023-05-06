fdd6d2aa729ad487b5976de2fc2c0f226b76baec
Jacob Colvin <jacobcolvin1@gmail.com>
2023-05-06 15:10:00 -0400

    Remove old files, fix license

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # homelab
 
 IaC and such for my homelab / personal cloud.
-
-## Getting Started
-
-- Set up ssh authorized key if needed.
-- Set TF vars, build cluster with TF.
-- Add a firewall.
-- Run `./connect.sh <user> <server>`
--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-kubectl --kubeconfig .kubeconfig apply -k applications/bootstrap
-
-echo "Sleeping so that bootstrapping has time to complete..."
-
-sleep 30
-
-kubectl --kubeconfig .kubeconfig apply -f applications/overlays/hcloud/appset.yaml
