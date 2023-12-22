0f6a32130c13b10aff9b785ec7defd5592e80233
Jacob Colvin <jacobcolvin1@gmail.com>
2023-12-22 00:03:11 -0500

    Improve talos bootstrap parameterization

--- talos/bootstrap.sh
+++ talos/bootstrap.sh
@@ -5,12 +5,14 @@ doppler run -p talhelper -c main talhelper genconfig
 talosctl --talosconfig=./clusterconfig/talosconfig config endpoint https://kube.home.macro.network:6443
 talosctl config merge ./clusterconfig/talosconfig
 
-talosctl apply-config -i -n knode01.home.macro.network -f clusterconfig/home-knode01.home.macro.network.yaml
+INITIAL_NODE=knode02.home.macro.network
+
+talosctl apply-config -i -n $INITIAL_NODE -f clusterconfig/home-$INITIAL_NODE.yaml
 
 echo "Waiting..."
 sleep 60
 
-talosctl bootstrap -e kube.home.macro.network -n knode01.home.macro.network
+talosctl bootstrap -e kube.home.macro.network -n $INITIAL_NODE
 
 echo "Waiting..."
 sleep 60
@@ -22,10 +24,10 @@ sleep 20
 
 kubectl kustomize extras/cilium/ --enable-helm | kubectl apply -f -
 kubectl kustomize extras/kubelet-csr-approver/ --enable-helm | kubectl apply -f -
-kubectl kustomize extras/argocd/ --enable-helm | kubectl apply -f -
 
 doppler run -p talhelper -c main envsubst < extras/doppler/secrets.yaml | kubectl apply -f -
 
 kubectl certificate approve $(kubectl get csr --sort-by=.metadata.creationTimestamp | grep Pending | awk '{print $1}')
 
-kubectl kustomize extras/argocd-apps/ | kubectl apply -f -
+# kubectl kustomize extras/argocd/ --enable-helm | kubectl apply -f -
+# kubectl kustomize extras/argocd-apps/ | kubectl apply -f -
