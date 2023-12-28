41e7d3bd8e0854f95cd5a328e1a8f9cf564919b0
Jacob Colvin <jacobcolvin1@gmail.com>
2023-12-27 20:18:26 -0500

    Add firewall rules blocking some inter-VLAN traffic

--- terraform/home/modules/unifi/ipv6.tf
+++ terraform/home/modules/unifi/ipv6.tf
@@ -5,7 +5,7 @@ resource "unifi_firewall_rule" "ipv6_allow_icmpv6_destination_unreachable" {
   name             = "IPv6 Allow ICMPv6 Destination Unreachable"
   action           = "accept"
   ruleset          = "WANv6_IN"
-  rule_index       = 26000
+  rule_index       = 25000
   protocol_v6      = "icmpv6"
   icmp_v6_typename = "destination-unreachable"
 }
@@ -14,7 +14,7 @@ resource "unifi_firewall_rule" "ipv6_allow_icmpv6_packet_too_big" {
   name             = "IPv6 Allow ICMPv6 Packet Too Big"
   action           = "accept"
   ruleset          = "WANv6_IN"
-  rule_index       = 26001
+  rule_index       = 25001
   protocol_v6      = "icmpv6"
   icmp_v6_typename = "packet-too-big"
 }
@@ -23,7 +23,7 @@ resource "unifi_firewall_rule" "ipv6_allow_icmpv6_time_exceeded" {
   name             = "IPv6 Allow ICMPv6 Time Exceeded"
   action           = "accept"
   ruleset          = "WANv6_IN"
-  rule_index       = 26002
+  rule_index       = 25002
   protocol_v6      = "icmpv6"
   icmp_v6_typename = "time-exceeded"
 }
@@ -32,7 +32,7 @@ resource "unifi_firewall_rule" "ipv6_allow_icmpv6_parameter_problem" {
   name             = "IPv6 Allow ICMPv6 Parameter Problem"
   action           = "accept"
   ruleset          = "WANv6_IN"
-  rule_index       = 26003
+  rule_index       = 25003
   protocol_v6      = "icmpv6"
   icmp_v6_typename = "parameter-problem"
 }
@@ -41,7 +41,7 @@ resource "unifi_firewall_rule" "ipv6_allow_icmpv6_echo_request" {
   name             = "IPv6 Allow ICMPv6 Echo Request"
   action           = "accept"
   ruleset          = "WANv6_IN"
-  rule_index       = 26004
+  rule_index       = 25004
   protocol_v6      = "icmpv6"
   icmp_v6_typename = "echo-request"
 }
@@ -50,7 +50,7 @@ resource "unifi_firewall_rule" "ipv6_allow_icmpv6_echo_response" {
   name             = "IPv6 Allow ICMPv6 Echo Response"
   action           = "accept"
   ruleset          = "WANv6_IN"
-  rule_index       = 26005
+  rule_index       = 25005
   protocol_v6      = "icmpv6"
   icmp_v6_typename = "echo-reply"
 }
--- terraform/home/modules/unifi/lan.tf
+++ terraform/home/modules/unifi/lan.tf
@@ -1,3 +1,39 @@
+locals {
+  default_lan = { default = { name = "Default", id = 0 } }
+  lans = {
+    guest = {
+      name    = "Guest"
+      id      = 5
+      purpose = "guest"
+    }
+    lab_management = {
+      name = "Lab Management"
+      id   = 9
+    }
+    lab = {
+      name = "Lab"
+      id   = 10
+    }
+    iot = {
+      name = "IoT"
+      id   = 20
+    }
+  }
+  lans_unrestricted = {
+    main = {
+      name = "Main"
+      id   = 1
+    }
+  }
+  lans_reservation = {
+    k8s_services = {
+      name = "K8s Services"
+      id   = 112
+      mask = 12
+    }
+  }
+}
+
 resource "unifi_network" "lan_default" {
   name    = "Default"
   purpose = "corporate"
@@ -26,32 +62,6 @@ resource "unifi_network" "lan_default" {
     ## Changes to DHCPv6 settings are not currently reflected in the controller.
     ##
     ignore_changes = [dhcp_v6_enabled]
-  }
-}
-
-locals {
-  lans = {
-    main = {
-      name = "Main"
-      id   = 1
-    }
-    guest = {
-      name    = "Guest"
-      id      = 5
-      purpose = "guest"
-    }
-    lab_management = {
-      name = "Lab Management"
-      id   = 9
-    }
-    lab = {
-      name = "Lab"
-      id   = 10
-    }
-    iot = {
-      name = "IoT"
-      id   = 20
-    }
   }
 }
 
@@ -90,6 +100,62 @@ resource "unifi_network" "lan" {
   }
 }
 
+resource "unifi_network" "lan_unrestricted" {
+  for_each = local.lans_unrestricted
+
+  name    = each.value.name
+  purpose = lookup(each.value, "purpose", "corporate")
+
+  subnet        = "10.${each.value.id}.0.0/16"
+  domain_name   = var.domain_name
+  multicast_dns = true
+  vlan_id       = (1000 + each.value.id)
+
+  dhcp_enabled    = true
+  dhcp_start      = "10.${each.value.id}.128.2"
+  dhcp_stop       = "10.${each.value.id}.255.254"
+  dhcp_v6_enabled = true
+  dhcp_v6_start   = "::2"
+  dhcp_v6_stop    = "::7d1"
+
+  ipv6_interface_type    = "pd"
+  ipv6_pd_interface      = "wan"
+  ipv6_pd_start          = "::2"
+  ipv6_pd_stop           = "::7d1"
+  ipv6_ra_enable         = true
+  ipv6_ra_priority       = "high"
+  ipv6_ra_valid_lifetime = 0
+  # Hex doesn't play nice with padding
+  ipv6_pd_prefixid = length(format("%x", each.value.id)) > 1 ? format("%x", each.value.id) : format("0%x", each.value.id)
+
+  lifecycle {
+    ## Changes to DHCPv6 settings are not currently reflected in the controller.
+    ##
+    ignore_changes = [dhcp_v6_enabled]
+  }
+}
+
+resource "unifi_network" "lan_reservation" {
+  for_each = local.lans_reservation
+
+  name    = each.value.name
+  purpose = "corporate"
+
+  subnet      = "10.${each.value.id}.0.0/${each.value.mask}"
+  domain_name = var.domain_name
+  vlan_id     = (1000 + each.value.id)
+
+  dhcp_enabled    = false
+  dhcp_v6_enabled = false
+
+  ipv6_interface_type = "pd"
+  ipv6_pd_interface   = "wan"
+  ipv6_pd_prefixid    = format("%x", each.value.id)
+  ipv6_ra_enable      = false
+  ipv6_pd_start       = "::2"
+  ipv6_pd_stop        = "::7d1"
+}
+
 resource "unifi_port_profile" "lan" {
   for_each = local.lans
 
@@ -102,40 +168,53 @@ resource "unifi_port_profile" "lan" {
   }
 }
 
-resource "unifi_network" "lan_k8s_services" {
-  name    = "K8s Services"
-  purpose = "corporate"
+resource "unifi_port_profile" "lan_unrestricted" {
+  for_each = local.lans_unrestricted
 
-  subnet      = "10.112.0.0/12"
-  domain_name = var.domain_name
-  vlan_id     = 1112
+  name                  = each.value.name
+  native_networkconf_id = unifi_network.lan_unrestricted[each.key].id
+  poe_mode              = "auto"
 
-  dhcp_enabled    = false
-  dhcp_v6_enabled = false
-
-  ipv6_interface_type = "pd"
-  ipv6_pd_interface   = "wan"
-  ipv6_pd_prefixid    = format("%x", 112)
-  ipv6_ra_enable      = false
-  ipv6_pd_start       = "::2"
-  ipv6_pd_stop        = "::7d1"
+  lifecycle {
+    ignore_changes = [forward]
+  }
 }
 
-resource "unifi_network" "lan_k8s_pods" {
-  name    = "K8s Pods"
-  purpose = "corporate"
+resource "unifi_firewall_rule" "allow_related_established" {
+  for_each = merge(local.default_lan, local.lans_unrestricted)
 
-  subnet      = "10.128.0.0/14"
-  domain_name = var.domain_name
-  vlan_id     = 1128
+  name       = "Allow related and established to ${each.value.name}"
+  action     = "accept"
+  ruleset    = "LAN_IN"
+  protocol   = "all"
+  rule_index = 20000 + each.value.id
 
-  dhcp_enabled    = false
-  dhcp_v6_enabled = false
+  dst_network_id = merge({ default = unifi_network.lan_default }, unifi_network.lan_unrestricted)[each.key].id
 
-  ipv6_interface_type = "pd"
-  ipv6_pd_interface   = "wan"
-  ipv6_pd_prefixid    = format("%x", 128)
-  ipv6_ra_enable      = false
-  ipv6_pd_start       = "::2"
-  ipv6_pd_stop        = "::7d1"
+  state_established = true
+  state_related     = true
+}
+
+resource "unifi_firewall_rule" "allow_traffic" {
+  for_each = merge(local.default_lan, local.lans_unrestricted)
+
+  name       = "Allow traffic from ${each.value.name}"
+  action     = "accept"
+  ruleset    = "LAN_IN"
+  protocol   = "all"
+  rule_index = 21000 + each.value.id
+
+  src_network_id = merge({ default = unifi_network.lan_default }, unifi_network.lan_unrestricted)[each.key].id
+}
+
+resource "unifi_firewall_rule" "drop_traffic" {
+  for_each = merge(local.lans, local.lans_unrestricted)
+
+  name       = "Drop traffic to ${each.value.name}"
+  action     = "drop"
+  ruleset    = "LAN_IN"
+  protocol   = "all"
+  rule_index = 22000 + each.value.id
+
+  dst_network_id = merge(unifi_network.lan, unifi_network.lan_unrestricted)[each.key].id
 }
