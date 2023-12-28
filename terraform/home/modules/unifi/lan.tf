428f18b42e949f2cd2f31bf52f96ccf9a88da4b9
Jacob Colvin <jacobcolvin1@gmail.com>
2023-12-27 21:56:00 -0500

    Combine normal and unrestricted unifi resources

--- terraform/home/modules/unifi/lan.tf
+++ terraform/home/modules/unifi/lan.tf
@@ -31,6 +31,11 @@ locals {
       id   = 112
       mask = 12
     }
+    k8s_pods = {
+      name = "K8s Pods"
+      id   = 128
+      mask = 14
+    }
   }
 }
 
@@ -66,42 +71,7 @@ resource "unifi_network" "lan_default" {
 }
 
 resource "unifi_network" "lan" {
-  for_each = local.lans
-
-  name    = each.value.name
-  purpose = lookup(each.value, "purpose", "corporate")
-
-  subnet        = "10.${each.value.id}.0.0/16"
-  domain_name   = var.domain_name
-  multicast_dns = true
-  vlan_id       = (1000 + each.value.id)
-
-  dhcp_enabled    = true
-  dhcp_start      = "10.${each.value.id}.128.2"
-  dhcp_stop       = "10.${each.value.id}.255.254"
-  dhcp_v6_enabled = true
-  dhcp_v6_start   = "::2"
-  dhcp_v6_stop    = "::7d1"
-
-  ipv6_interface_type    = "pd"
-  ipv6_pd_interface      = "wan"
-  ipv6_pd_start          = "::2"
-  ipv6_pd_stop           = "::7d1"
-  ipv6_ra_enable         = true
-  ipv6_ra_priority       = "high"
-  ipv6_ra_valid_lifetime = 0
-  # Hex doesn't play nice with padding
-  ipv6_pd_prefixid = length(format("%x", each.value.id)) > 1 ? format("%x", each.value.id) : format("0%x", each.value.id)
-
-  lifecycle {
-    ## Changes to DHCPv6 settings are not currently reflected in the controller.
-    ##
-    ignore_changes = [dhcp_v6_enabled]
-  }
-}
-
-resource "unifi_network" "lan_unrestricted" {
-  for_each = local.lans_unrestricted
+  for_each = merge(local.lans, local.lans_unrestricted)
 
   name    = each.value.name
   purpose = lookup(each.value, "purpose", "corporate")
@@ -138,7 +108,7 @@ resource "unifi_network" "lan_unrestricted" {
 resource "unifi_network" "lan_reservation" {
   for_each = local.lans_reservation
 
-  name    = each.value.name
+  name    = "_${each.value.name}"
   purpose = "corporate"
 
   subnet      = "10.${each.value.id}.0.0/${each.value.mask}"
@@ -157,22 +127,10 @@ resource "unifi_network" "lan_reservation" {
 }
 
 resource "unifi_port_profile" "lan" {
-  for_each = local.lans
+  for_each = merge(local.lans, local.lans_unrestricted)
 
   name                  = each.value.name
   native_networkconf_id = unifi_network.lan[each.key].id
-  poe_mode              = "auto"
-
-  lifecycle {
-    ignore_changes = [forward]
-  }
-}
-
-resource "unifi_port_profile" "lan_unrestricted" {
-  for_each = local.lans_unrestricted
-
-  name                  = each.value.name
-  native_networkconf_id = unifi_network.lan_unrestricted[each.key].id
   poe_mode              = "auto"
 
   lifecycle {
@@ -189,7 +147,7 @@ resource "unifi_firewall_rule" "allow_related_established" {
   protocol   = "all"
   rule_index = 20000 + each.value.id
 
-  dst_network_id = merge({ default = unifi_network.lan_default }, unifi_network.lan_unrestricted)[each.key].id
+  dst_network_id = merge({ default = unifi_network.lan_default }, unifi_network.lan)[each.key].id
 
   state_established = true
   state_related     = true
@@ -204,7 +162,7 @@ resource "unifi_firewall_rule" "allow_traffic" {
   protocol   = "all"
   rule_index = 21000 + each.value.id
 
-  src_network_id = merge({ default = unifi_network.lan_default }, unifi_network.lan_unrestricted)[each.key].id
+  src_network_id = merge({ default = unifi_network.lan_default }, unifi_network.lan)[each.key].id
 }
 
 resource "unifi_firewall_rule" "drop_traffic" {
@@ -216,5 +174,5 @@ resource "unifi_firewall_rule" "drop_traffic" {
   protocol   = "all"
   rule_index = 22000 + each.value.id
 
-  dst_network_id = merge(unifi_network.lan, unifi_network.lan_unrestricted)[each.key].id
+  dst_network_id = merge(unifi_network.lan, unifi_network.lan)[each.key].id
 }
