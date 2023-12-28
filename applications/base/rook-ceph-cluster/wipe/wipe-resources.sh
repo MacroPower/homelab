bca6a2e80f6c7c712b096d50f8b50d5d60d55a2e
Jacob Colvin <jacobcolvin1@gmail.com>
2023-12-28 14:07:57 -0500

    Fix rook-ceph wipe scripts

--- applications/base/rook-ceph-cluster/wipe/wipe-resources.sh
+++ applications/base/rook-ceph-cluster/wipe/wipe-resources.sh
@@ -7,3 +7,9 @@ kubectl delete app rook-ceph-operator -n argocd
 kubectl patch CephBlockPool ceph-blockpool -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
 kubectl patch CephFileSystem ceph-filesystem -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
 kubectl patch CephObjectStore ceph-objectstore -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
+
+kubectl patch Secret rook-ceph-mon -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch ConfigMap rook-ceph-mon-endpoints -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
+
+kubectl patch CephFilesystemSubVolumeGroup ceph-filesystem-csi -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch CephCluster rook-ceph -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
--- applications/base/rook-ceph-cluster/wipe/wipe-rook.sh
+++ applications/base/rook-ceph-cluster/wipe/wipe-rook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ROOK_WORKERS="knode01 knode02 knode03 knode13 knode14 knode15"
+ROOK_WORKERS="knode13 knode14 knode15"
 if ! test "$*" == ""; then
   ROOK_WORKERS="$*"
 fi
@@ -7,7 +7,6 @@ fi
 for i in $ROOK_WORKERS; do
   echo "Wiping $i"
   cat wipe-rook.yaml | sed -e "s/HOSTNAME/$i/g" | kubectl apply -f -
-  # cat wipe-rook-shell.yaml | sed -e "s/HOSTNAME/$i/g" | kubectl apply -f -
 done
 
 sleep 1
@@ -15,5 +14,6 @@ sleep 1
 for i in $ROOK_WORKERS; do
   echo "Waiting for $i to complete"
   kubectl wait --timeout=900s --for=jsonpath='{.status.phase}=Succeeded' pod disk-wipe-$i -n default
-  kubectl delete pod disk-wipe-$i
+  # kubectl logs disk-wipe-$i -n default
+  # kubectl delete pod disk-wipe-$i
 done
