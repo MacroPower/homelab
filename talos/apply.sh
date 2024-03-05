2fc9a27f3823cc34a18d23365d5f4efead420772
Jacob Colvin <jacobcolvin1@gmail.com>
2024-03-05 00:06:29 -0500

    Add initial taskfile

--- talos/apply.sh
+++
@@ -1,5 +0,0 @@
-doppler run -p talhelper -c main -- talhelper validate talconfig
-
-doppler run -p talhelper -c main -- talhelper genconfig --no-gitignore
-
-eval $(doppler run -p talhelper -c main -- talhelper gencommand apply)
--- talos/bootstrap-ks.sh
+++
@@ -1,7 +0,0 @@
-kubectl kustomize extras/cilium/ --enable-helm | kubectl apply -f -
-kubectl kustomize extras/kubelet-csr-approver/ --enable-helm | kubectl apply -f -
-
-doppler run -p talhelper -c main envsubst < extras/doppler/secrets.yaml | kubectl apply -f -
-
-kubectl kustomize extras/argocd/ --enable-helm | kubectl apply -f -
-kubectl kustomize extras/argocd-apps/ --enable-helm | kubectl apply -f -
