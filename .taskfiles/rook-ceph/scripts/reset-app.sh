ca8e6edf54ba8acb3f397fb626ff98785a437a63
Jacob Colvin <jacobcolvin1@gmail.com>
2024-06-29 18:10:46 -0400

    Fix rook-ceph reset tasks

--- .taskfiles/rook-ceph/scripts/reset-app.sh
+++ .taskfiles/rook-ceph/scripts/reset-app.sh
@@ -1,15 +1,14 @@
 kubectl patch app rook-ceph-cluster -n argocd -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge --type merge
-kubectl delete app rook-ceph-cluster -n argocd
-
-kubectl patch app rook-ceph-operator -n argocd -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge --type merge
-kubectl delete app rook-ceph-operator -n argocd
+kubectl delete app rook-ceph-cluster -n argocd --grace-period=0
 
 kubectl patch CephBlockPool ceph-blockpool -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
 kubectl patch CephFileSystem ceph-filesystem -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
 kubectl patch CephObjectStore ceph-objectstore -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch CephFilesystemSubVolumeGroup ceph-filesystem-csi -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl patch CephCluster rook-ceph -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
+
+kubectl patch app rook-ceph-operator -n argocd -p '{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}' --type merge --type merge
+kubectl delete app rook-ceph-operator -n argocd --wait=false
 
 kubectl patch Secret rook-ceph-mon -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
 kubectl patch ConfigMap rook-ceph-mon-endpoints -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
-
-kubectl patch CephFilesystemSubVolumeGroup ceph-filesystem-csi -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
-kubectl patch CephCluster rook-ceph -n rook-ceph -p '{"metadata":{"finalizers":null}}' --type=merge
